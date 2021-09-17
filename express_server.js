90cfd91479f35098c3801c8b017ce79104a71ac2
Kevin Nelson <kevin.nelson@mail.utoronto.ca>
2021-09-17 00:19:51 +0000

    Change so only logged in users can see their own urls

--- express_server.js
+++ express_server.js
@@ -11,8 +11,22 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userID" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "user2ID"}
+};
+
+const urlFinder = (ID) => {
+  let result = {};
+  for ( url in urlDatabase) {
+    if (urlDatabase[url].userID === ID) {
+      result[url] = { 
+        longURL: urlDatabase[url].longURL,
+        userID: ID
+      }
+      //urlDatabase[url].longURL;
+    };
+  };
+  return result
 };
 
 const users = {
@@ -26,7 +40,7 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-sink"
   }
-}
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -41,18 +55,22 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const userid = req.cookies["user_id"]
+  const userid = req.cookies["user_id"];
+  if (!userid) {
+    return res.redirect("/login");
+  };
+  const filteredDB = urlFinder(userid);
   const templateVars = { 
     user_id: userid,
     user: users[userid],
-    urls: urlDatabase,
+    urls: filteredDB,
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   if (!req.cookies["user_id"]) {
-    res.redirect("/login").end
+    return res.redirect("/login");
   }
   const userid = req.cookies["user_id"]
   const templateVars = { 
@@ -66,7 +84,7 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL] = {longURL};
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -74,15 +92,16 @@ app.get("/urls/:shortURL", (req, res) => {
   const userid = req.cookies["user_id"];
   const templateVars = { 
     shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL],
-    user_id: userid
+    longURL: urlDatabase[req.params.shortURL].longURL,
+    user_id: userid,
+    user: users[userid],
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
@@ -95,7 +114,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL/update", (req, res) => {
   const shortURL = req.params.shortURL;
   const editedURL = req.body.text;
-  urlDatabase[shortURL] = editedURL;
+  urlDatabase[shortURL].longURL = editedURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -119,14 +138,22 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars);
 })
 
-app.post("/login", (req, res) => {
+const findUser = (email, password) => {
   for (const object in users) {
-    if (users[object].email === req.body.email && users[object].password === req.body.password) {
-      res.cookie("user_id", object);
-      return res.redirect("/urls",);
-    } else {
-      res.status(403).send("Incorrect email/password").end;
-    };
+    if (users[object].email === email && users[object].password === password) {
+      return object;
+    } 
+  };
+}
+
+app.post("/login", (req, res) => {
+  const userID = findUser(req.body.email, req.body.password)
+  if (userID) {
+    res.cookie("user_id", userID);
+    return res.redirect("/urls",);
+  }
+  else {
+    return res.status(403).send("Incorrect email/password");
   };
 });
 
@@ -139,7 +166,7 @@ app.post("/register", (req, res) => {
   const user = generateRandomString();
   for (const object in users) {
     if (users[object].email === req.body.email) {
-      res.status(400).send("Email is already registered").end
+      return res.status(400).send("Email is already registered");
     }
   };
   users[user] = {
